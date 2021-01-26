72bb67bac7f33b9ddbe0ab1cd0cd5fcceeefddeb
Osvaldas Bernatavičius <osvaldas.bernatavicius@gmail.com>
2021-01-26 18:58:45 +0200

    Interceptor added.

--- web/src/main/java/osvaldas/games/config/WebConfig.java
+++ web/src/main/java/osvaldas/games/config/WebConfig.java
@@ -1,8 +1,10 @@
 package osvaldas.games.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import osvaldas.games.interceptor.RequestInterceptor;
 import osvaldas.games.util.ViewNames;
 
 @Configuration
@@ -11,5 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName(ViewNames.HOME);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestInterceptor());
     }
 }
